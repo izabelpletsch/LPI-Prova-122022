0167f4598b4c073b265deedd95175c1bcc492e62
santostks <brun.santos244@fatec.sp.gov.br>
2022-12-13 11:19:32 -0300

    Último commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodosComponent } from './todos/todos.component';
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodosComponent
+    TodosComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -5,24 +5,27 @@ import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Todo } from './todo';
+import { MessageService } from './message.service';
 
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
 
-  private todosUrl = 'https://jsonplaceholder.typicode.com/todos';  // URL to web api
+  private todosUrl = 'api/todos';  // URL to web api
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
 
   constructor(
-    private http: HttpClient) { }
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   /** GET todos from the server */
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(this.todosUrl)
       .pipe(
+        tap(_ => this.log('fetched todos')),
         catchError(this.handleError<Todo[]>('getTodos', []))
       );
   }
@@ -35,6 +38,7 @@ export class TodoService {
         map(todos => todos[0]), // returns a {0|1} element array
         tap(h => {
           const outcome = h ? 'fetched' : 'did not find';
+          this.log(`${outcome} todo id=${id}`);
         }),
         catchError(this.handleError<Todo>(`getTodo id=${id}`))
       );
@@ -44,6 +48,7 @@ export class TodoService {
   getTodo(id: number): Observable<Todo> {
     const url = `${this.todosUrl}/${id}`;
     return this.http.get<Todo>(url).pipe(
+      tap(_ => this.log(`fetched todo id=${id}`)),
       catchError(this.handleError<Todo>(`getTodo id=${id}`))
     );
   }
@@ -55,6 +60,9 @@ export class TodoService {
       return of([]);
     }
     return this.http.get<Todo[]>(`${this.todosUrl}/?name=${term}`).pipe(
+      tap(x => x.length ?
+         this.log(`found todos matching "${term}"`) :
+         this.log(`no todos matching "${term}"`)),
       catchError(this.handleError<Todo[]>('searchTodos', []))
     );
   }
@@ -64,6 +72,7 @@ export class TodoService {
   /** POST: add a new todo to the server */
   addTodo(todo: Todo): Observable<Todo> {
     return this.http.post<Todo>(this.todosUrl, todo, this.httpOptions).pipe(
+      tap((newTodo: Todo) => this.log(`added todo w/ id=${newTodo.id}`)),
       catchError(this.handleError<Todo>('addTodo'))
     );
   }
@@ -73,6 +82,7 @@ export class TodoService {
     const url = `${this.todosUrl}/${id}`;
 
     return this.http.delete<Todo>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted todo id=${id}`)),
       catchError(this.handleError<Todo>('deleteTodo'))
     );
   }
@@ -80,6 +90,7 @@ export class TodoService {
   /** PUT: update the todo on the server */
   updateTodo(todo: Todo): Observable<any> {
     return this.http.put(this.todosUrl, todo, this.httpOptions).pipe(
+      tap(_ => this.log(`updated todo id=${todo.id}`)),
       catchError(this.handleError<any>('updateTodo'))
     );
   }
@@ -97,8 +108,16 @@ export class TodoService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
+  }
+
+  /** Log a TodoService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`TodoService: ${message}`);
   }
 }
